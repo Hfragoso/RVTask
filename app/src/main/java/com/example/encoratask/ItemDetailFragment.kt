5cf99081b712cc5e70c789fc33bed09356e45ebe
MauDev <mauricio.munoz87@gmail.com>
2021-09-07 22:45:39 -0500

    Instead of load the DialogFragment in a new activity. Now is loading in the current activity and then call the service to get the info to load.

--- app/src/main/java/com/example/encoratask/ItemDetailFragment.kt
+++ app/src/main/java/com/example/encoratask/ItemDetailFragment.kt
@@ -1,13 +1,17 @@
 package com.example.encoratask
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.example.encoratask.dummy.DummyContent
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.encoratask.model.Character
+import com.example.encoratask.viewmodel.CharacterInfoViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.item_detail.*
 
 /**
  * A fragment representing a single Item detail screen.
@@ -15,12 +19,12 @@ import com.example.encoratask.dummy.DummyContent
  * in two-pane mode (on tablets) or a [ItemDetailActivity]
  * on handsets.
  */
-class ItemDetailFragment : Fragment() {
+class ItemDetailFragment : BottomSheetDialogFragment() {
 
     /**
      * The dummy content this fragment is presenting.
      */
-    private var item: DummyContent.DummyItem? = null
+    private var item_id: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +34,8 @@ class ItemDetailFragment : Fragment() {
                 // Load the dummy content specified by the fragment
                 // arguments. In a real-world scenario, use a Loader
                 // to load content from a content provider.
-                item = DummyContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
-                activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = item?.content
+                item_id = it.getLong(ARG_ITEM_ID)
+                //activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = item?.content
             }
         }
     }
@@ -40,12 +44,34 @@ class ItemDetailFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.item_detail, container, false)
 
-        // Show the dummy content as text in a TextView.
-        item?.let {
-            rootView.findViewById<TextView>(R.id.item_detail).text = it.details
+        val characterInfo = ViewModelProviders.of(this).get(CharacterInfoViewModel::class.java)
+        characterInfo.getCharacterInfo(item_id!!).observe(this.viewLifecycleOwner, Observer { info -> info?.let {
+            if(info != null){
+                loadInformation(info)
+            }
         }
 
+        })
+        // Show the dummy content as text in a TextView.
+        /*item?.let {
+            rootView.findViewById<TextView>(R.id.item_detail).text = it.details
+        }*/
+
         return rootView
+    }
+
+    private fun loadInformation(info: Character){
+        item_name.text = info.name
+        item_gender.text = info.gender
+        item_species.text = info.species
+        item_status.text = info.status
+        item_type.text = info.type
+
+        Glide.with(this)
+            .load(info.image)
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .into(item_image)
+
     }
 
     companion object {
@@ -54,5 +80,13 @@ class ItemDetailFragment : Fragment() {
          * represents.
          */
         const val ARG_ITEM_ID = "item_id"
+
+        @JvmStatic
+        fun newInstance(item_id: Long?) =
+            ItemDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putLong(ARG_ITEM_ID, item_id!!)
+                }
+            }
     }
 }
